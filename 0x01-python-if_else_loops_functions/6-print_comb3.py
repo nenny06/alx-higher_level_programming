d930fea9109be3bac867c749854533c3cc033d17
Nnenna <nnennauzo55@gmail.com>
2023-03-13 02:59:24 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(1, 10):
-        if x >= y:
-            continue
-        elif x == 8 and y == 9:
-            print("{}{}".format(x,y))
-        else:
-            print("{}{}".format(x,y), end=",")
+number = 0
+while number <= 89:
+    if number % 10 == 0:
+        number += 1 + number // 10
+    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
+    number += 1
