7798161eb65e1614693fbce324a225eff68c1bc1
Nnenna <nnennauzo55@gmail.com>
2023-03-13 01:55:11 -0700

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for x in range(0, 100):
-    if x == 99:
-        print("{}".format(x), end=",")
+for num in range(00, 100):
+    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
